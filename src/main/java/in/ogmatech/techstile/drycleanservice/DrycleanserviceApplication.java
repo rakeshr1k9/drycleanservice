54a02e167c250ffad499075c42a5adbc106b5608
rakeshr1k9 <rakeshr1k9@gmail.com>
2018-04-21 12:23:26 +0530

    spring boot initializer

---
+++ src/main/java/in/ogmatech/techstile/drycleanservice/DrycleanserviceApplication.java
@@ -0,0 +1,12 @@
+package in.ogmatech.techstile.drycleanservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrycleanserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrycleanserviceApplication.class, args);
+	}
+}
